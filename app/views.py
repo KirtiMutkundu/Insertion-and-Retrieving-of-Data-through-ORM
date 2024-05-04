0a17c69d6616676e7f2cdb4b98dd38fbe3db1d5d
KirtiMutkundu <kirtimutkundureddy2007@gmail.com>
2024-05-04 13:15:44 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,71 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+from app.models import *
+from django.db.models.functions import Length
+
+def insert_topic(request):
+    tn=input('enter topic name')
+    TO=Topic.objects.filter(topic_name=tn)
+    TO.save()
+    d={'QLTO':Topic.objects.all()}
+    #return HttpResponse('Topic is created Successfully')
+    return render(request,'display_topics.html',d)
+
+def insert_webpage(request):
+    tn=input('enter topicname')
+    n=input('enter name')
+    u=input('enter url')
+    e=input('enter email')
+
+   
+    LTO=Topic.objects.filter(topic_name=tn)
+    if LTO:
+        TO=LTO[0]
+        WO=Webpage.objects.get_or_create(topic_name=TO,name=n,url=u,email=e)[0]
+        WO.save()
+        d={'QLWO':Webpage.objects.all()}
+        #return HttpResponse('Webpage is created successfully')
+        return render(request,'display_webpages.html',d)
+    #else:
+        #return HttpRespnse('Given Topic is not present in my Parent Table')
+
+def insert_access(request):
+    i=int(input('enter id of webpage object'))
+    
+    d=input('enter date')
+    a=input('enter author') 
+    WO=Webpage.objects.get(id=i)
+    AO=AccessRecord.objects.get_or_create(name=WO,date=d,author=a)[0]
+    AO.save()
+    d={'QLAO':Webpage.objects.all()}
+    #return HttpResponse('Accessrecord is created successfully')
+    return render(request,'display_accessrecord.html',d)
+
+
+def display_topics(request):
+    QLTO=Topic.objects.all()
+    d={'QLTO':QLTO}
+    return render(request,'display_topics.html',context=d)
+
+def display_webpages(request):
+    QLWO=Webpage.objects.all()
+    QLWO=Webpage.objects.filter(topic_name='Cricket')
+    QLWO=Webpage.objects.exclude(topic_name='Cricket')
+    #QLWO=Webpage.objects.get(topic_name='Football') #get() returned more than one Webpage -- it returned 2!
+    QLWO=Webpage.objects.all()[ 2:: ]
+    QLWO=Webpage.objects.all().order_by('name')
+    QLWO=Webpage.objects.all().order_by('-name')
+    QLWO=Webpage.objects.all().order_by(Length ('name'))
+    QLWO=Webpage.objects.all().order_by(Length ('name').desc())
+
+    d={'QLWO':QLWO}
+    return render(request,'display_webpages.html', d)
+
+def display_accessrecord(request):
+    QLAO=AccessRecord.objects.all()
+    #QLAO=AccessRecord.objects.all
+    d={'QLAO':QLAO}
+    return render(request,'display_accessrecord.html', d)
+    
